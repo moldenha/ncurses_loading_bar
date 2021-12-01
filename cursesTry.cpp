b0802c8a261fa572d86462496ad586812958d9f7
moldenha <60318757+moldenha@users.noreply.github.com>
2021-12-01 16:18:43 -0500

    Add files via upload

---
+++ cursesTry.cpp
@@ -0,0 +1,49 @@
+#include "load.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include <iostream>
+#include <unistd.h>
+#include <ncurses.h>
+#include <thread>
+
+//g++ main.cpp -o ctest -std=c++11 -pthread -lncurses -Incurses
+
+std::string gen_random(const int len) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    std::string tmp_s;
+    tmp_s.reserve(len);
+
+    for (int i = 0; i < len; ++i) {
+        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    
+    return tmp_s;
+}
+void testFunc(bar *b, int objects, int sleep, int randCharacters, std::string ending){
+	std::string numStr = std::to_string(objects);
+	int back = 4+(numStr.size()*2)+ending.size();
+	b->updateObjects(objects);
+	b->updateBack(back);
+	for(int i = 0; i < objects; i++){
+		std::string rand = "[+] "+gen_random(randCharacters)+"\n";
+		b->write(rand);
+		b->update(ending);
+		usleep(sleep);
+	}
+}
+
+
+int main(){
+	load main(2, 30, true);
+	std::thread t1 = std::thread(testFunc, main.getBar(0), 50, 200000, 10, "EDB1");
+	std::thread t2 = std::thread(testFunc, main.getBar(1), 500, 10000, 16, "EDB2");
+	t1.join();
+	t2.join();	
+	main.end();
+	return 0;
+}
---
+++ load.h
@@ -0,0 +1,217 @@
+#ifndef LOAD_H
+#define LOAD_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <ncurses.h>
+#include <assert.h>
+
+class bar{
+	private:
+		int _line, _witdth, _objects, maxx, _current, _y, maxy;
+		int * written;
+	public:
+		WINDOW *win, *writeWin;
+		bar(int line, int width, int objects, int *wr=NULL, WINDOW *w = NULL);
+		bar(int line, bool all, int objects, int back=20, int *wr=NULL, WINDOW *w = NULL);
+		std::string construct(float progress, std::string end);
+		void updateObjects(int objects);
+		void updateLine(int line);
+		void updateBack(int back);
+		void update(std::string end);
+		void write(std::string wr);
+		void update();
+		void finished();
+		void finished(std::string);
+		void clear();
+		void clear(std::string);
+		void end();
+};
+
+bar::bar(int line, int width, int objects, int *wr, WINDOW *w)
+	:_line(line),
+       	_witdth(width),
+       	_objects(objects),
+	_current(0),
+	writeWin(w),
+	written(wr)
+{
+	maxx = getmaxx(stdscr);
+	maxy = getmaxy(stdscr);
+	int y;
+	y = getcury(stdscr);
+	_y = y;
+	if(writeWin == NULL)
+		_line = (line + y);
+	else
+		_line = (maxy-(line+1));
+	win = newwin(1, maxx, _line, 0);
+}
+
+bar::bar(int line, bool all, int objects, int back, int *wr, WINDOW *w)
+	:_line(line),
+       	_objects(objects),
+	_current(0),
+	writeWin(w),
+	written(wr)
+{
+	initscr();
+	maxx = getmaxx(stdscr);
+	maxy = getmaxy(stdscr);
+	_witdth = maxx-back;
+	int y;
+	y = getcury(stdscr);
+	_y = y;
+	if(writeWin == NULL)
+		_line = (line + y);
+	else
+		_line = (maxy-(line+1));
+	//window, (height, width, y, x)
+	win = newwin(1, maxx, _line, 0);
+}
+
+void bar::write(std::string wr){
+	assert(writeWin != NULL);
+	if(*written >= getmaxy(writeWin)){
+		wclear(writeWin);
+		wrefresh(writeWin);
+		(*written) = 0;
+	}
+	(*written)++;
+	wprintw(writeWin, wr.c_str());
+	wrefresh(writeWin);
+}
+
+void bar::updateObjects(int objects){
+	_objects = objects;
+}
+
+void bar::updateLine(int line){
+	assert(writeWin == NULL);	
+	_line = line + _y;
+	mvwin(win, _line, 0);
+	wrefresh(win);
+}
+
+void bar::updateBack(int back){
+	_witdth = maxx-back;
+}
+
+void bar::update(){
+	update("");
+}
+
+void bar::update(std::string end){
+	_current++;
+	if(_current <= _objects){
+	std::string ending = "|"+std::to_string(_current)+"/"+std::to_string(_objects)+"|"+end;
+	float progress = (float)_current/(float)_objects;
+	std::string toPrint = construct(progress, ending);
+	clear(toPrint);
+	}
+	else
+		finished();
+}
+
+void bar::clear(){
+	wclear(win);
+	wrefresh(win);
+}
+
+void bar::clear(std::string put){
+	wclear(win);
+	waddstr(win, put.c_str());
+	wrefresh(win);
+}
+
+void bar::finished(std::string toPrint){
+	clear(toPrint);
+	//getch();
+}
+
+void bar::finished(){
+	clear("FINISHED!");
+	//getch();
+}
+
+void bar::end(){
+	delwin(win);
+}
+
+std::string bar::construct(float progress, std::string end){
+	std::string output;
+	output += "[";
+	int pos = _witdth * progress;
+	for (int i = 0; i < _witdth; ++i) {
+		if (i < pos) output += "=";
+		else if (i == pos) output += ">";
+		else output += " ";
+	}
+	output += end;
+	return output;
+}
+
+
+class load{
+	private:
+		std::vector<bar*> bars;
+		WINDOW *writeWin;
+		int _bars;
+		int written;
+	public:
+		load(int barAmt, bool write=false);
+		load(int barAmt, int objects, bool write=false);
+		void end();
+		bar *getBar(int b){return bars.at(b);}
+};
+
+load::load(int barAmt, bool write)
+	:_bars(barAmt),
+	written(0)
+{
+	initscr();
+	if(write == true){
+		int width = getmaxx(stdscr);
+		int maxy = getmaxy(stdscr);
+		int height = maxy-(barAmt+1);
+		//window, (height, width, y, x)
+		writeWin = newwin(height, width, 0, 0);
+	}
+	else
+		writeWin = NULL;
+
+	for(int i = 0; i < barAmt; i++){
+		bars.push_back(new bar(i, true, 30, &written, writeWin));
+	}
+}
+
+load::load(int barAmt, int objects, bool write)
+	:_bars(barAmt),
+	written(0)
+{
+	initscr();
+	if(write == true){
+		int width = getmaxx(stdscr);
+		int maxy = getmaxy(stdscr);
+		int height = maxy-(barAmt+1);
+		//window, (height, width, y, x)
+		writeWin = newwin(height, width, 0, 0);
+	}
+	else
+		writeWin = NULL;
+	for(int i = 0; i < barAmt; i++){
+		bars.push_back(new bar(i, true, objects, &written, writeWin));
+	}
+}
+
+
+void load::end(){
+	for(int i = 0; i < bars.size(); i++){
+		bars.at(i)->end();
+		free(bars.at(i));
+	}
+	endwin();
+}
+
+#endif
